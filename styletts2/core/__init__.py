e175dd93cb1929e942841bdbeb435847b0dc735b
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2026-09-18 17:29:35 -0400

    refactor: move the train() implementation from cli to core

--- styletts2/core/__init__.py
+++ styletts2/core/__init__.py
@@ -1,3 +1,4 @@
 from .preprocess import PREPROCESS_CATEGORIES, load_config, preprocess
+from .train import train
 
-__all__ = ["load_config", "preprocess", "PREPROCESS_CATEGORIES"]
+__all__ = ["load_config", "preprocess", "PREPROCESS_CATEGORIES", "train"]
